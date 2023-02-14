09e27ca3735fdc6a2627fc1cf095b877c8d4322f
Pratik Devidas Ingole <94819856+Ingole987@users.noreply.github.com>
2023-02-14 20:03:38 +0530

    Update timezonedb.cs

--- timezonedb.cs
+++ timezonedb.cs
@@ -61,3 +61,47 @@ static string GetResponseString(HttpListenerRequest request)
     return responseString;
 }
 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8080/");
+            listener.Start();
+            Console.WriteLine("Listening to Postman calls at http://localhost:8080/");
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                string responseString = GetResponseString(request);
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                HttpListenerResponse response = context.Response;
+                response.ContentLength64 = buffer.Length;
+                response.ContentType = "application/json";
+                Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+            }
+        }
+
+        static string GetResponseString(HttpListenerRequest request)
+        {
+            var info = new Dictionary<string, string>();
+            info.Add("Request URL", request.Url.ToString());
+            info.Add("Request Method", request.HttpMethod);
+            info.Add("Request Headers", request.Headers.ToString());
+            info.Add("Request Content Type", request.ContentType);
+
+            string json = JsonConvert.SerializeObject(info, Formatting.Indented);
+            return json;
+        }
+    }
+}
