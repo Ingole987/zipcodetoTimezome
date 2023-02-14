a7d074fac6194e1f3cd09b853880a9cb14353d42
Pratik Devidas Ingole <94819856+Ingole987@users.noreply.github.com>
2023-02-14 19:40:08 +0530

    Update timezonedb.cs

--- timezonedb.cs
+++ timezonedb.cs
@@ -16,3 +16,36 @@ namespace TimeZoneExample
         }
     }
 }
+
+
+
+using System;
+using System.IO;
+using System.Net;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8080/");
+            listener.Start();
+            Console.WriteLine("Listening to Postman calls at http://localhost:8080/");
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                string responseString = "Hello World!";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                HttpListenerResponse response = context.Response;
+                response.ContentLength64 = buffer.Length;
+                Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+            }
+        }
+    }
+}
+
