29fa9c28c60b4d23759cf28dc3d62188d792a0c3
Pratik Devidas Ingole <94819856+Ingole987@users.noreply.github.com>
2023-02-20 11:30:34 +0530

    Create 33.cs

---
+++ 33.cs
@@ -0,0 +1,49 @@
+public static List<EmailModel> DataIngetion()
+        {
+            string sqlConnection = "Data Source=icapdissqlserver.database.windows.net;database=icapdisql_dev;uid=icapdis;password=Provana@123;";
+
+            using (SqlConnection con = new SqlConnection(sqlConnection))
+            {
+                using (SqlCommand cmd = new SqlCommand("select top 1 * From ClientInputConfigs", con))
+                {
+                    //cmd.CommandType = System.Data.CommandType.Text;
+
+                    con.Open();
+                    using (SqlDataReader dataReader = cmd.ExecuteReader())
+                    {
+                        List<EmailModel> emailModels = new List<EmailModel>();
+                        while (dataReader.Read())
+                        {
+                            EmailModel emailModel = new EmailModel();
+                            emailModel.title = "Test Email by  Pratik";
+                            //emailModel.message = "This is automated email from .Net Code";
+                            emailModel.pipilineName = "Deployment: Dev-Test";
+                            emailModel.date = DateTime.Now;
+                            emailModel.color = "green";
+                            emailModel.addressTo = "pratikdevidas.ingole@provana.com";
+                            //emailModel.addressCC = "purnachandra.majhi@provana.com";
+                            emailModel.ClientName = dataReader["ClientName"].ToString();
+                            emailModel.ClientSource = dataReader["ClientSource"].ToString();
+                            emailModel.StartDate = dataReader["StartDate"].ToString();
+                            emailModel.EndDate = dataReader["EndDate"].ToString();
+
+                            //emailModel.message = "ClientName : " + emailModel.ClientName + "||"+
+                            //    "ClientSource : " + emailModel.ClientSource + "||" +
+                            //    "StartDate : " + emailModel.StartDate + "||" +
+                            //    "EndDate : " + emailModel.EndDate;
+
+                            emailModel.message = ("<!DOCTYPE html><html>" +
+                                                 "<table width=\"1000\" border=\"1\" cellspacing=\"0\" cellpadding=\"0\" style=\"border:1px solid #ccc;\"> <tr align=\"center\"> </table>" +
+                                                 "<th> ClientName </th><th> ClientSource  </th> <th> StartDate</th> <th> EndDate </th> " +
+                                                 "<tr  align=\"center\"></tr>" +
+                                                 $"<td> {emailModel.ClientName} '</td> <td> {emailModel.ClientSource} </td> <td>{ emailModel.StartDate}</td> <td>{emailModel.EndDate}</td>");
+                                                 ;
+
+                            emailModels.Add(emailModel);
+                        }
+
+                        return emailModels;
+                    }
+                }
+
+            }
